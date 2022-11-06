919861d0ebab2014b54e2bba07d8a59ae06cd31d
Jiho Lee <optional.int@kakao.com>
2022-11-06 17:42:51 +0900

    feat: write locustfile structure

--- locustfile.py
+++ locustfile.py
@@ -1,7 +1,35 @@
-from locust import HttpUser, task
+from locust import FastHttpUser, task
 
 
-class So1sUser(HttpUser):
+class So1sUser(FastHttpUser):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.model = {}
+        self.deployments = []
+
+    def on_start(self):
+        self.login()
+
+    def login(self):
+        self.client.post(
+            "/api/v1/signin", {"username": "so1s", "password": "admin12345"})
+
+    def create_model(self):
+        pass
+
+    def delete_model(self):
+        pass
+
+    def create_deployments(self):
+        pass
+
+    def delete_deployments(self):
+        pass
+
+    def load_endpoints(self):
+        pass
+
     @task
-    def hello_world(self):
+    def main_task(self):
         self.client.post("/livez")
